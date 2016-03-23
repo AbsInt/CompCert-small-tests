4da608e9eedd63005b4effd39cc2057beaf2aec3
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2016-03-23 10:54:12 +0100

    Fix a bug in the pre-parser.

--- regression/parsing.c
+++ regression/parsing.c
@@ -160,6 +160,24 @@ void (*krk(a, b, c))(int)
   return aa;
 }
 
+int hhh(int());
+
+int (testparen)(int T) {
+  return T;
+}
+
+int (testparen2(int T)) {
+  return T;
+}
+
+int ((testparen3)(int T)) {
+  return T;
+}
+
+int ((((((((((testparen10))))))))))(int T) {
+  return T;
+}
+
 int main () {
   f(g);
   i();
